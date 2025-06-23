5305dca1b95217421c3f5374cb849d6a2407688c
AsahiTokunaga <hihimamug14@gmail.com>
2025-06-23 21:11:46 +0900

    bot init

---
+++ src/handler.rs
@@ -0,0 +1,12 @@
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+use serenity::async_trait;
+
+pub struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{}が起動しました", ready.user.name)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+use serenity::prelude::*;
+use dotenv::dotenv;
+
+mod handler;
+use handler::Handler;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let token = dotenv::var("TOKEN").expect("[ FAILED ] トークンが設定されていません");
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(&token, intents).event_handler(Handler).await.expect("[ FAILED ] botの起動に失敗しました");
+
+    if let Err(e) = client.start().await {
+        println!("[ FAILED ] botの起動に失敗しました: {}", e);
+    }
+}
